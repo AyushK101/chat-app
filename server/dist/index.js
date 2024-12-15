482ab07014e087f44c6514a86159d89016680761
Ayush Kumar <socialacc211@gmail.com>
2024-12-15 22:26:20 +0530

    Delete server/dist directory

--- server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const constants_1 = require("./constants");
-const db_1 = __importDefault(require("./db/db"));
-(async () => await (0, db_1.default)(constants_1.MONGO_URI))();
-//# sourceMappingURL=index.js.map
